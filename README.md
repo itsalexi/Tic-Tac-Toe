daca8a60691ee3c1568fa8a3fd989f9f3f9ac56f
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-07-23 12:36:31 +0800

    added code for win condition

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tic-Tac-Toe
+
 A tic-tac-toe game built with HTML, CSS, JS!
--- main.js
+++ main.js
@@ -1,6 +1,58 @@
 const boardDiv = document.querySelector(".game");
 const gameBoard = (function () {
   let board = ["", "", "", "", "", "", "", "", ""];
+  let gameActive = false;
+  let turns = 0;
+
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  function startGame() {
+    gameActive = true;
+    gameBoard.renderBoard();
+    playerController.addPlayer("Jack", "x");
+    playerController.addPlayer("Jerry", "O");
+  }
+
+  function checkWin() {
+    let roundWon = false;
+    for (let i = 0; i < winConditions.length; i++) {
+      const currentWinCondition = winConditions[i];
+      const a = board[currentWinCondition[0]];
+      const b = board[currentWinCondition[1]];
+      const c = board[currentWinCondition[2]];
+
+      // this checks to see if there is a match, if there is no match, just continue
+      if (a === "" || b === "" || c == "") {
+        continue;
+      }
+
+      // if there is a match then round is won.
+      if (a === b && b == c) {
+        roundWon = true;
+        break;
+      }
+    }
+    if (roundWon) {
+      console.log("Won");
+      gameActive = false;
+    }
+
+    // if there is still no winner, and there has been 9 turns then its a tie
+    if (!roundWon && turns == 9) {
+      console.log("Tie");
+      gameActive = false;
+    }
+  }
+
   function renderBoard() {
     for (let i = 0; i < board.length; i++) {
       const tile = document.createElement("div");
@@ -13,6 +65,8 @@ const gameBoard = (function () {
 
   function resetBoard() {
     board = ["", "", "", "", "", "", "", "", ""];
+    gameActive = false;
+    roundWon = false;
     updateBoard();
   }
 
@@ -22,10 +76,17 @@ const gameBoard = (function () {
   }
 
   function updateTile(event) {
-    console.dir(`${event.target.textContent}`);
-    if (event.target.textContent == "") {
-      event.target.textContent = playerController.getCurrentPlayer();
+    if (gameActive) {
+      if (event.target.textContent == "") {
+        player = playerController.getCurrentPlayer();
+        event.target.textContent = player;
+        board[event.target.dataset.index] = player;
+
+        checkWin();
+        turns++;
+      }
     }
+
     // event.target.dataset.index
   }
 
@@ -35,6 +96,9 @@ const gameBoard = (function () {
     resetBoard,
     updateBoard,
     updateTile,
+    startGame,
+    gameActive,
+    turns,
   };
 })();
 
@@ -73,9 +137,7 @@ const playerController = (function () {
   };
 })();
 
-gameBoard.renderBoard();
-playerController.addPlayer("Jack", "x");
-playerController.addPlayer("Jerry", "O");
+gameBoard.startGame();
 
 const tiles = document.querySelectorAll(".tile");
 tiles.forEach((element) =>
