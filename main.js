329d4a037c6471a4cea492fd3e0f063a5446d446
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-07-23 12:07:02 +0800

    Added a player controller to make players

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const boardDiv = document.querySelector(".game");
-
 const gameBoard = (function () {
   let board = ["", "x", "o", "", "", "", "", "", ""];
-
   function renderBoard() {
     for (let i = 0; i < board.length; i++) {
       const tile = document.createElement("div");
@@ -23,11 +21,46 @@ const gameBoard = (function () {
     renderBoard();
   }
 
+  function updateTile(event) {
+    console.dir(`${event.target.textContent}`);
+    if (event.target.textContent == "") {
+      console.log("Good");
+    }
+    // event.target.dataset.index
+  }
+
   return {
     board,
     renderBoard,
     resetBoard,
+    updateBoard,
+    updateTile,
   };
 })();
 
+const playerController = (function () {
+  let players = [];
+
+  const playerFactory = (name, character) => {
+    return { name, character };
+  };
+
+  function addPlayer(name, character) {
+    const player = playerFactory(name, character.toUpperCase());
+    players.push(player);
+  }
+
+  return {
+    players,
+    addPlayer,
+  }
+})();
+
 gameBoard.renderBoard();
+playerController.addPlayer("Jack", "x");
+playerController.addPlayer("Jerry", "O");
+
+const tiles = document.querySelectorAll(".tile");
+tiles.forEach((element) =>
+  element.addEventListener("click", (event) => gameBoard.updateTile(event))
+);
