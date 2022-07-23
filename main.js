41af6d96284540217e5d74ff5f97776fa1c1e13d
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-07-23 14:27:18 +0800

    Display for players and scoring system, turn text

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const boardDiv = document.querySelector(".game");
 const gameBoard = (function () {
+  const resetButton = document.getElementById("resetButton");
+  const turnText = document.getElementById("turnText");
+
   let board = ["", "", "", "", "", "", "", "", ""];
   let gameActive = false;
   let roundWon = false;
+  let roundTie = false;
 
   let turns = 0;
 
@@ -23,6 +27,11 @@ const gameBoard = (function () {
     addListeners();
     playerController.addPlayer("Jack", "x");
     playerController.addPlayer("Jerry", "O");
+    playerController.updatePlayers();
+    playerController.players[0].turn = true;
+    playerController.players[1].turn = false;
+
+    turnText.textContent = `It's ${playerController.players[0].name}'s turn!`;
   }
 
   function addListeners() {
@@ -59,6 +68,7 @@ const gameBoard = (function () {
     if (!roundWon && turns == 9) {
       console.log("Tie");
       gameActive = false;
+      roundTie = true;
     }
   }
 
@@ -76,8 +86,11 @@ const gameBoard = (function () {
     board = ["", "", "", "", "", "", "", "", ""];
     gameActive = false;
     roundWon = false;
+    roundTie = false;
     turns = 0;
     boardDiv.innerHTML = "";
+    turnText.textContent = `It's ${playerController.players[0].name}'s turn!`;
+    resetButton.textContent = "Restart?";
     startGame();
   }
 
@@ -90,12 +103,39 @@ const gameBoard = (function () {
         turns++;
         console.log(turns);
         checkWin();
+        changeGameInfo(player);
+
+        if (roundWon) {
+          playerController.addScore(player);
+        }
       }
     }
 
     // event.target.dataset.index
   }
+  function changeGameInfo(player) {
+    let nameIndex;
+    let winNameIndex;
+    player === "X" ? (nameIndex = 1) : (nameIndex = 0);
+    turnText.textContent = `It's ${playerController.players[nameIndex].name}'s turn!`;
+    changeReset();
 
+    if (roundWon) {
+      player === "X" ? (winNameIndex = 0) : (winNameIndex = 1);
+      turnText.textContent = `${playerController.players[winNameIndex].name} won!`;
+    }
+    if (roundTie) {
+      turnText.textContent = "It was a tie!";
+    }
+  }
+
+  function changeReset() {
+    if (!gameActive) {
+      resetButton.textContent = "Play again?";
+    }
+  }
+
+  resetButton.addEventListener("click", resetBoard);
   return {
     board,
     renderBoard,
@@ -110,15 +150,41 @@ const gameBoard = (function () {
 const playerController = (function () {
   let players = [];
 
+  const player1Score = document.getElementById("player1Score");
+  const player2Score = document.getElementById("player2Score");
+
+  const player1Name = document.getElementById("player1Name");
+  const player2Name = document.getElementById("player2Name");
+
   const playerFactory = (name, character) => {
     let turn = false;
+    let score = 0;
     character == "X" ? (turn = true) : (turn = false);
-    return { name, character, turn };
+    return { name, character, turn, score };
   };
 
   function addPlayer(name, character) {
     const player = playerFactory(name, character.toUpperCase());
     players.push(player);
+  }
+
+  function addScore(player) {
+    if (players[0].character == player) {
+      players[0].score++;
+    } else {
+      players[1].score++;
+    }
+    updateScore();
+  }
+
+  function updatePlayers() {
+    player1Name.textContent = players[0].name;
+    player2Name.textContent = players[1].name;
+  }
+
+  function updateScore() {
+    player1Score.textContent = `Score: ${players[0].score}`;
+    player2Score.textContent = `Score: ${players[1].score}`;
   }
 
   function getCurrentPlayer() {
@@ -139,6 +205,9 @@ const playerController = (function () {
     players,
     addPlayer,
     getCurrentPlayer,
+    updateScore,
+    updatePlayers,
+    addScore,
   };
 })();
 
